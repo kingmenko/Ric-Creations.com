913c3be9a8bc1427c5895237c739deea531184f9
kingmenko <114106522+kingmenko@users.noreply.github.com>
2022-10-26 18:10:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+<script>
+let todo1= 'be nice!';
+let todo2= 'be helpful!';
+let todo6= 'be you!';
+
+let element = document.createElement('div');element.innerText = todo6;
+document.body.appendChild(element);
+
+</script>
+
+
+
+
+
+
+
+
